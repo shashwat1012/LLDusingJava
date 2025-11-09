472258d7822150836c723986f2cd3a9affe2934b
shashwat1012 <shashwat.shivam.eee21@itbhu.ac.in>
2025-11-09 21:42:10 +0530

    Added problem statement for Composite Design Pattern

---
+++ CompositeDesignPattern/ProblemStatement/PSMain.java
@@ -0,0 +1,17 @@
+package CompositeDesignPattern.ProblemStatement;
+
+public class PSMain {
+    
+    public static void main(String args[]) {
+
+        Directory movieDirectory = new Directory("Movies");
+        File JohnWick = new File("JohnWick");
+        movieDirectory.add(JohnWick);
+
+        Directory actionMovies = new Directory("ActionMovies");
+        File Creed = new File("Creed");
+        actionMovies.add(Creed);
+        movieDirectory.add(actionMovies);
+        movieDirectory.ls();
+    }
+}
